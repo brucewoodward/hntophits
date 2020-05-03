564d296a0653e7da5ba0f602c6523befe21dc817
Bruce Woodward <bruce.woodward@gmail.com>
2020-05-03 18:14:07 +1000

    fix; time frame bug

--- app/helpers/stories_front_page_helper.rb
+++ app/helpers/stories_front_page_helper.rb
@@ -1,7 +1,7 @@
 module StoriesFrontPageHelper
 
   def underlined_element? time_frame
-    return 'underlined' if !params.has_key? :time_frame
+    return 'underlined' if !params.has_key? :time_frame and time_frame == 'week'
     return 'underlined' if params[:time_frame] == time_frame
   end
 
