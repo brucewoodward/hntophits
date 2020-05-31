2df93090a67ef5f95065972038987c511246f755
Bruce Woodward <bruce.woodward@gmail.com>
2020-05-31 13:08:18 +1000

    updrading to 5.2

--- Gemfile
+++ Gemfile
@@ -2,8 +2,11 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.7'
-gem 'actionview', '~> 5.1.7'
+gem 'rails', '~> 5.2', '>= 5.2.4.3'
+
+gem 'bootsnap', '~> 1.4'
+
+#gem 'actionview', '~> 5.1.7'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem "puma", ">= 4.3.5"
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Hnhits
   class Application < Rails::Application
-    config.load_defaults 5.1
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
+
+Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
